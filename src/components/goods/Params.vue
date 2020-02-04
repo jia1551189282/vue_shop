52cacd18244ddf372dc47be15fe97c8a3198d067
z小佳 <1551189282@qq.com>
2020-02-04 21:00:29 +0800

    商品参数

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -8,20 +8,141 @@
     <!-- 卡片视图区域 -->
     <el-card>
       <el-alert title="注意:只允许为第三方分类设置相关参数！" type="warning" :closable="false"></el-alert>
-    </el-card>
+      <!-- 选择商品分类的区域 -->
+      <el-row class="cat_opt">
+        <el-col>
+          <span>选择商品分类：</span>
+          <!-- 选择商品分类的级联选择框 -->
+          <el-cascader
+            expand-trigger="hover"
+            :options="catelist"
+            :props="cateProps"
+            v-model="selectedCateKeys"
+          ></el-cascader>
+        </el-col>
+      </el-row>
 
-    <!-- 选择商品分类的区域 -->
-    <el-row class="cat_opt">
-      <el-col>
-        <span>选择商品分类：</span>
-        <!-- 选择商品分类的级联选择框 -->
-      </el-col>
-    </el-row>
+      <!-- 页签区域 -->
+      <el-tabs v-model="activeName" @tab-click="handleTabClick">
+        <!-- 添加动态属性的面板 -->
+        <el-tab-pane label="动态参数" name="many">
+          <el-button type="primary" size="mini" :disabled="isDisabledBtn">添加参数</el-button>
+          <el-table :data="manyTableData" border stripe>
+            <el-table-column type="expand"></el-table-column>
+            <el-table-column type="index"></el-table-column>
+            <el-table-column label="参数名称" prop="attr_name"></el-table-column>
+            <el-table-column label="操作">
+              <template>
+                <el-button size="mini" type="primary">编辑</el-button>
+                <el-button size="mini" type="danger">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-tab-pane>
+        <el-tab-pane label="静态属性" name="second">
+          <el-button type="primary" size="mini" :disabled="isDisabledBtn">添加属性</el-button>
+
+          <el-table :data="onlyTableData" border stripe>
+            <el-table-column type="expand"></el-table-column>
+            <el-table-column type="index"></el-table-column>
+            <el-table-column label="属性名称" prop="attr_name"></el-table-column>
+            <el-table-column label="操作">
+              <template>
+                <el-button size="mini" type="primary">编辑</el-button>
+                <el-button size="mini" type="danger">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-tab-pane>
+      </el-tabs>
+    </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      catelist: [],
+      cateProps: {
+        value: 'cat_id',
+        label: 'cat_name',
+        children: 'children'
+      },
+      // 级联选择框双向数据绑定到的数组
+      selectedCateKeys: [],
+      // 被激活的页签的名称
+      activeName: 'many',
+      // 动态参数的数据
+      manyTableData: [],
+      // 静态参数的数据
+      onlyTableData: [],
+      // 控制添加对话框的显示与隐藏
+      addDialogVisible: false
+    }
+  },
+  created() {
+    this.getCateList()
+  },
+  methods: {
+    async getCateList() {
+      const { data: res } = await this.$http.get('categories')
+      if (res.meta.status !== 200) {
+        return this.$message.console.error('获取商品分类失败')
+      }
+      this.catelist = res.data
+      console.log(this.catelist)
+    },
+    // 级联选择框选中项变化，会触发这个函数
+    handleChange() {
+      this.getParamsData()
+    },
+    async getParamsData() {
+      // 证明选中的不是三级分类
+      if (this.selectedCateKeys.length !== 3) {
+        this.selectedCateKeys = []
+        return
+      }
+      // 证明选中的是三级分类
+      console.log(this.selectedCateKeys)
+      // 根据所选分类的id 和当前所处的面板,获取对应的参数
+      const { data: res } = await this.$http.get(
+        `categories/${this.cateId}/attributes`,
+        {
+          params: { sel: this.activeName }
+        }
+      )
+
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取参数列表失败!')
+      }
+      console.log(res.data)
+      if (this.activeName === 'many') {
+        this.manyTableData = res.data
+      } else {
+        this.onlyTableData = res.data
+      }
+    },
+    handleTabClick() {
+      console.log(this.activeName)
+    }
+  },
+  computed: {
+    isDisabledBtn() {
+      if (this.selectedCateKeys.length !== 3) {
+        return true
+      }
+      return false
+    },
+    // 当前选中的三级分类的id
+    cateId() {
+      if (this.selectedCateKeys.length === 3) {
+        return this.selectedCateKeys[2]
+      }
+      return null
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
